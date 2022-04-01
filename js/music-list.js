ce7d170bc8766bdc579e7727d983fc91cd2973d6
tunggf1711 <ngthtungg1711@gmail.com>
2022-04-01 10:42:31 +0700

    điền nội dung commit vào đây

---
+++ js/music-list.js
@@ -0,0 +1,39 @@
+
+let allMusic = [
+    {
+      name: "Cà Phê - Min",
+      artist: "Min",
+      img: "caphe",
+      src: "caphe"
+    },
+    {
+      name: "Đúng Cũng Thành Sai - Mỹ Tâm",
+      artist: "Mỹ Tâm",
+      img: "dungcungthanhsai",
+      src: "dungcungthanhsai"
+    },
+    {
+      name: "Gieo Quẻ - Hoàng Thuỳ Linh",
+      artist: "Hoàng Thuỳ Linh",
+      img: "gieoque",
+      src: "gieoque"
+    },
+    {
+      name: "Không Trọn Vẹn Nữa - Châu Khải Phong",
+      artist: "Châu Khải Phong",
+      img: "khongtronvennua",
+      src: "khongtronvennua"
+    },
+    {
+      name: "Ngày Đầu Tiên - Đức Phúc",
+      artist: "Đức Phúc",
+      img: "ngaydautien",
+      src: "ngaydautien"
+    },
+    {
+      name: "BlackJack - Soobin,Binz",
+      artist: "Soobin,Binz",
+      img: "blackjack",
+      src: "blackjack"
+    }
+  ];
